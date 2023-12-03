c4d4e2d56e45c833af4867c3a6ef34a8eb1c0867
LuisReyesP <relujo@hotmail.com>
2023-12-03 10:37:50 -0500

    Hi from the template commit-msg hook

--- config/config.js
+++ config/config.js
@@ -17,9 +17,8 @@
 const config = {
   env: process.env.NODE_ENV || "development",
   port: process.env.PORT || 3001,
-  jwtSecret: process.env.JWT_SECRET || "YOUR_secret_key",
-  mongoUri:
-    "mongodb+srv://relujo:1095reyes@cluster0.wonxugf.mongodb.net/pf-henry?retryWrites=true&w=majority",
+  jwtSecret: process.env.JWT_SECRET,
+  mongoUri: process.env.mongoUri,
 };
 
 export default config;
