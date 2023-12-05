a744ca175f235b123a3d7dc443c61db2a70b284a
LuisReyesP <relujo@hotmail.com>
2023-12-05 11:29:59 -0500

    Hi from the template commit-msg hook

--- config/config.js
+++ config/config.js
@@ -1,19 +1,4 @@
 // this is for local connection to mongodb
-// const config = {
-//   env: process.env.NODE_ENV || "development",
-//   port: process.env.PORT || 3001,
-//   jwtSecret: process.env.JWT_SECRET || "YOUR_secret_key",
-//   mongoUri:
-//     process.env.MONGODB_URI ||
-//     process.env.MONGO_HOST ||
-//     "mongodb://" +
-//       (process.env.IP || "localhost") +
-//       ":" +
-//       (process.env.MONGO_PORT || "27017") +
-//       "/mernproject",
-// };
-
-// this is mongodb Atlas connection
 const config = {
   env: process.env.NODE_ENV || "development",
   port: process.env.PORT || 3001,
@@ -21,5 +6,14 @@ const config = {
   mongoUri:
     "mongodb+srv://relujo:1095reyes@cluster0.wonxugf.mongodb.net/pf-henry?retryWrites=true&w=majority",
 };
+
+// // this is mongodb Atlas connection
+// const config = {
+//   env: process.env.NODE_ENV,
+//   port: process.env.PORT
+//   jwtSecret: process.env.JWT_SECRET,
+//   mongoUri:
+//     "mongodb+srv://relujo:1095reyes@cluster0.wonxugf.mongodb.net/pf-henry?retryWrites=true&w=majority",
+// };
 
 export default config;
