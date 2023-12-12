185ca9074fad992a9ce6ff9b012ad2396490800d
LuisReyesP <relujo@hotmail.com>
2023-12-12 15:00:45 -0500

    modified the Product model so can save image files

--- server/routes/product.routes.js
+++ server/routes/product.routes.js
@@ -2,13 +2,18 @@ import express from "express";
 import userCtrl from "../controllers/user.controller.js";
 import authCtrl from "../controllers/auth.controller.js";
 import productCtrl from "../controllers/product.controller.js";
+import multer from "multer";
 
 const router = express.Router();
+
+//Upload image to mongo
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage });
 
 router
   .route("/api/product")
   .get(productCtrl.list)
-  .post(authCtrl.requireSignin, authCtrl.hasAuthorization, productCtrl.create);
+  .post(upload.single("photo"), productCtrl.create);
 
 router
   .route("/api/product/:productId")
