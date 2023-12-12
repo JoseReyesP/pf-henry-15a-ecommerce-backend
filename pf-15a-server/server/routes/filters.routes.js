adb71fbd449da6b37b09eede032dcd8b56cd39da
LuisReyesP <relujo@hotmail.com>
2023-12-11 21:08:39 -0500

    deleting the files and folders added by mistake

--- pf-15a-server/server/routes/filters.routes.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import filtersCtrl from "../controllers/filters.controller.js";
-import paginateCtrl from "../controllers/paginate.controller.js";
-
-const router = express.Router();
-
-router.route("/api/filter").get(filtersCtrl.filter, paginateCtrl.paginate);
-
-export default router;
--- pf-15a-server/server/routes/paginate.routes.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import paginateCtrl from "../controllers/paginate.controller.js";
-
-const router = express.Router();
-
-router.route("/api/paginado").get(paginateCtrl.paginate);
-
-export default router;
