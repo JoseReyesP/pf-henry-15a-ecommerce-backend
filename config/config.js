e69c5fa1bc7ee2ea99b23f94cd78e118326536b1
IsaacRB98 <isaacrbarboza98@gmail.com>
2023-12-23 12:53:09 -0300

    prueba de notificaciones con nodemailer

--- config/config.js
+++ config/config.js
@@ -1,23 +1,23 @@
 // this is for local connection to mongodb
-// const DBUser = "relujo";
-// const DBPassword = "1095reyes";
-// const DBUrl = `mongodb+srv://${DBUser}:${DBPassword}@cluster0.wonxugf.mongodb.net/pf-henry?retryWrites=true&w=majority`;
-// const config = {
-//  env: process.env.NODE_ENV || "development",
-//  port: process.env.PORT || 3001,
-//  jwtSecret: process.env.JWT_SECRET || "YOUR_secret_key",
-//  mongoUri: DBUrl,
-//  henrucciEmail: "henrucciwebsite@gmail.com",
-//};
+ const DBUser = "relujo";
+ const DBPassword = "1095reyes";
+ const DBUrl = `mongodb+srv://${DBUser}:${DBPassword}@cluster0.wonxugf.mongodb.net/pf-henry?retryWrites=true&w=majority`;
+ const config = {
+  env: process.env.NODE_ENV || "development",
+  port: process.env.PORT || 3001,
+  jwtSecret: process.env.JWT_SECRET || "YOUR_secret_key",
+  mongoUri: DBUrl,
+  henrucciEmail: "henrucciwebsite@gmail.com",
+};
 
 // this is mongodb Atlas connection
-const config = {
-   env: process.env.NODE_ENV,
-   port: process.env.PORT,
-   jwtSecret: process.env.JWT_SECRET,
-   mongoUri: process.env.mongoUri,
-   sgAPIKey: process.env.sgAPIKey,
-   herucciEmail: process.env.herucciEmail,
-};
+// const config = {
+//    env: process.env.NODE_ENV,
+//    port: process.env.PORT,
+//    jwtSecret: process.env.JWT_SECRET,
+//    mongoUri: process.env.mongoUri,
+//    sgAPIKey: process.env.sgAPIKey,
+//    herucciEmail: process.env.herucciEmail,
+// };
 
 export default config;
